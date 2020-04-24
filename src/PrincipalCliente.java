c87b3ba2411b0cd764aa886c886ba85cef4f67a0
Martin Morães <martin.moraes@gmail.com>
2020-04-23 22:24:09 -0300

    Tentativa de fazer rodar em thread.

--- src/PrincipalCliente.java
+++ src/PrincipalCliente.java
@@ -6,19 +6,17 @@ import java.util.Scanner;
 
 public class PrincipalCliente {
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Scanner teclado = new Scanner(System.in);
-		
-		
-//		Solicita uma conexão
-		Socket cliente = new Socket("127.0.0.1", 12345);
 
-//		Cria um canal de envio
+		Scanner teclado = new Scanner(System.in);
+
+		Socket cliente = new Socket("127.0.0.1", 12345);
 		PrintStream saida = new PrintStream(cliente.getOutputStream());
 
-//		Envia os pacotes
-		while (teclado.hasNextLine()) {
-				saida.println(teclado.nextLine());
-			}
+		String enviar = null;
+		while (teclado.hasNextLine() && enviar.equals("SAIR")) {
+			enviar = teclado.nextLine();
+			saida.println(enviar);
+		}
 
 	}
 
--- src/PrincipalServer.java
+++ src/PrincipalServer.java
@@ -6,16 +6,50 @@ import java.util.Scanner;
 public class PrincipalServer {
 
 	public static void main(String[] args) throws IOException {
-//		Cria um serviço Socket
+
 		ServerSocket server = new ServerSocket(12345);
-//		Cria um canal de comunicação
-		System.out.println("Antes do Accept");
-		Socket cliente = server.accept(); //blocante
-		System.out.println("Depois do Accept");
-//		Recebe pacotes do cliente
-		Scanner scanner = new Scanner(cliente.getInputStream()); 
-		String recebido = scanner.nextLine();
-		System.out.println(recebido);
+
+		do {
+			System.out.println("Aguardando nova conwxão.");
+			Socket cliente = server.accept();
+			new Atende(cliente).start();
+		} while (true);
+
+	}
+
+}
+
+class Atende extends Thread {
+	private Socket cliente;
+
+	public Atende(Socket cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public void run() {
+
+		String continuar = null;
+		try {
+
+			Scanner scanner = new Scanner(cliente.getInputStream());
+			System.out.println("Aguardando mensagem!");
+			String recebido;
+			while ((recebido = scanner.nextLine()) != null) {
+
+				System.out.println(recebido);
+			}
+
+			System.out.println("Encerrou!!!");
+
+//			while (scanner.hasNextLine() && continuar.equals("SAIR")) {
+//				String recebido = scanner.nextLine();
+//				System.out.println(recebido);
+//			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
